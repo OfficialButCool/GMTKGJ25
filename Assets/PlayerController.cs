92e4d180a59cb66de83898ba61e2e6feff85609b
OFFICIAL <umargov@icloud.com>
2025-07-31 11:21:10 +0100

    updated velocity things
    if this ends up not working go back to the last commit.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -21,11 +21,11 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float move = Input.GetAxisRaw(horizontalInput);
-        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
 
         if (Input.GetButtonDown(jumpInput) && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
         }
 
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.1f, groundLayer);
