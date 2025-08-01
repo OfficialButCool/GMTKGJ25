2abadb429826a1a75a441ee9558485b0eb94ffeb
OFFICIAL <umargov@icloud.com>
2025-08-01 21:50:08 +0100

    added respawn but not fixed.
    added respawns to fall damage but it respawns at the start of the scene not the checkpoint i alligned will fix tmrw.

--- Assets/falldamagerespawn.cs
+++ Assets/falldamagerespawn.cs
@@ -1,6 +1,33 @@
 using UnityEngine;
 
-public class falldamagerespawn
+public class PlayerRespawn : MonoBehaviour
 {
-    
+    public float fallThreshold = 10f; // How far below checkpoint before respawn
+    private Vector2 checkpoint;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        checkpoint = transform.position; // Start point
+    }
+
+    void Update()
+    {
+        if (transform.position.y < checkpoint.y - fallThreshold)
+        {
+            Respawn();
+        }
+    }
+
+    public void SetCheckpoint(Vector2 newCheckpoint)
+    {
+        checkpoint = newCheckpoint;
+    }
+
+    public void Respawn()
+    {
+        rb.velocity = Vector2.zero; // Reset motion
+        transform.position = checkpoint;
+    }
 }
